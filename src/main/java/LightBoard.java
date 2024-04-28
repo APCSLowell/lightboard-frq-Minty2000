fb86b0d5e4b243099292b3e75f923ae2f4c0ea11
Minty2000 <142567186+Minty2000@users.noreply.github.com>
2024-04-28 07:38:50 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -3,7 +3,8 @@ public class LightBoard
   /** The lights on the board, where true represents on and false represents off.
    */
   private boolean[][] lights;
-
+  private int Rows, Cols;
+  private int random;
   /** Constructs a LightBoard object having numRows rows and numCols columns.
    * Precondition: numRows > 0, numCols > 0
    * Postcondition: each light has a 40% probability of being set to on.
@@ -11,7 +12,20 @@ public class LightBoard
   public LightBoard(int numRows, int numCols)
   {
     /* to be implemented in part (a) */
-
+    Rows = numRows;
+    Cols = numCols;
+    random = Math.random()*101;
+         for (int r = 0;r<Rows;r++){
+           for(int c=0;c<Cols;c++){
+        if(random > 60 ){
+        lights[r][c] = false;
+        random = Math.random()*101;
+        }else {
+          light[r][c] = true;
+          random = Math.random()*101;
+        }
+}
+}
   }
 
   /** Evaluates a light in row index row and column index col and returns a status
@@ -21,8 +35,11 @@ public class LightBoard
   public boolean evaluateLight(int row, int col)
   {
     /* to be implemented in part (b) */
-   
- 
+      if(lights[row][col] == true){
+return true;
+      }else 
+        return false;
+
   }
   public boolean[][] getLights()
   {
